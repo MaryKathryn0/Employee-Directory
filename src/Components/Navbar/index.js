9e4b025915fff5c426617bd6c3580f71b0a21426
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-11-23 15:28:03 -0600

    Added components and navbar, table within that also created utils with api file

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+    return(
+        <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">Employee Directory</span>
+      </nav>
+    )
+}
